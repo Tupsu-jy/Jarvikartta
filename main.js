9b7cc50fd0dd6b54e4d5bcd1cd5d0ab1bddc7d09
nikitaessine <essine.nikita@gmail.com>
2019-10-09 11:28:17 +0300

    dedecde

---
+++ main.js
@@ -0,0 +1,40 @@
+const lakeAPI = "http://rajapinnat.ymparisto.fi/api/jarvirajapinta/1.0/odata/Jarvi"
+
+const map = L.map('map').setView([61.92, 25.74], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+const getLakes = async (lat, long) => {
+    console.log('got ' + lat + " " + long)
+    const latGt = lat - 0.10
+    const latLt = lat + 0.10
+    const longGt = long - 0.10
+    const longLt = long + 0.10
+
+    const url = lakeAPI + "?$top=100&$filter=KoordErLong gt '"+ longGt +"' and KoordErLong lt '"+ longLt +"' and KoordErLat gt '"+ latGt +"' and KoordErLat lt '"+ latLt +"'"
+    try {
+        const response = await fetch(url)
+        const responseJSON = await response.json()
+        return responseJSON
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const markLakes = async (lat, long) => {
+    const lakes = await getLakes(lat, long)
+    for (lake of lakes.value) {
+        console.log(lake)
+        let marker = L.marker([lake.KoordErLat, lake.KoordErLong]).addTo(map);
+        marker.bindPopup("<b>" + lake.Nimi + "</b><br>" + lake.KuntaNimi + "<br><a href='https://maps.google.com/?q=" + lake.KoordErLat + "," + lake.KoordErLong + "' target='!blank'>Google Maps</a>").openPopup();
+    }
+}
+
+map.on('click', function(e){
+    var coord = e.latlng;
+    var lat = coord.lat.toFixed(2);
+    var long = coord.lng.toFixed(2);
+    markLakes(lat, long)
+});
