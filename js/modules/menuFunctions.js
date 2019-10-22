8021d6a6986b6d034ed33d202f7c399befdf5ccf
Tupsu-jy <55530823+Tupsu-jy@users.noreply.github.com>
2019-10-22 19:48:34 +0300

    modal

---
+++ js/modules/menuFunctions.js
@@ -0,0 +1,17 @@
+//new AWN().modal('<b>Custom modal window message</b>')
+//import {  } from "../main.js"
+
+export const modalSaannostely = () => {
+    let tiedot="<b>asd</b>"
+    new AWN().modal(tiedot);
+}
+
+export const modalInfo = () => {
+    let tiedot="<b>asd</b>"
+    new AWN().modal(tiedot);
+}
+
+export const modalOhje = () => {
+    let tiedot="<b>asd</b>"
+    new AWN().modal(tiedot);
+}
